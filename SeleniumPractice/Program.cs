bcb477194fcb3c82e0535d4b8dbe43f533b965ab
sbhattacherya <sbhattacherya@gmail.com>
2018-12-18 22:18:00 -0500

    Changes done
    Good commit

---
+++ SeleniumPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Selenium\ChromeDriver");
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.Quit();
+        }
+    }
+}
